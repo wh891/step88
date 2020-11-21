643aa9608739824a074003b07d580c1a62f6fb83
5w891 <58629942+wh891@users.noreply.github.com>
2020-11-21 22:59:26 +0800

    Add files via upload

---
+++ tem.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+#hai subscribe dulu channel 5wwe baru boleh recode
+r="\033[1;31m" # merah
+g="\033[1;32m" # hijau
+y="\033[1;33m" # kuning
+b="\033[1;34m" # biru
+p="\033[1;35m" # ungu
+cy="\033[1;36m" # biru muda
+w="\033[1;37m" # putih
+clear
+echo $r"jangan lupa subscribe 5WWE yah"
+sleep 2
+echo $g"udh subscribe 5WWE? (y/n) : "
+read y
+sleep 3
+echo $r"terimakasih yah semoga kamu sehat selalu"
+sleep 3
+clear
+
+echo $g"===============5WWE==============="
+echo $r"Channel : 5WWE"
+echo $r"Website : aril891.freevar.com"
+echo $r"Github : wh891"
+echo $r"Nomor : 081937567454"
+echo $r"Akun fb : aril arikku"
+echo $r"Author : 5WWE"
+echo $g"===============5WWE==============="
+sleep 5
+
+echo $p"(*) $cy"1 $b"install advphishing"
+echo $p"(*) $cy"2 $b"install darkfb premium"
+echo $p"(*) $cy"3 $b"install virus Xz"
+echo $p"(*) $cy"4 $b"install virus Err0r-ICA"
+echo $p"(*) $cy"5 $b"install virus spesial"
+echo $p"(*) $cy"6 $b"install KK&KTP Palsu"
+echo $p"(*) $cy"99 $b"exit"
+read -p ">『5ωཌaRiL』vvip꧂ : " pil;
+if [ $pil = 1 ]
+then
+sleep 2
+clear
+cd $home
+pkg update && pkg upgrade
+pkg install git
+pkg install php
+git clone https;//github.com/ignitect/advphishing
+cd advphishing
+./Android-Setup.sh
+./AdvPhishing.sh
+fi
+#hiraukan
+if [ $pil = 2 ]
+then
+sleep 2
+clear
+cd $home
+pkg update && pkg upgrade
+pkg install git
+fi
+#xh
+if [ $pil = 3 ]
+then
+pkg update && pkg upgrade
+pkg install git
+pkg install python
+pkg install python2
+git clone https://github.com/TSMaitry/VirusX.git
+cd VirusX
+chmod 777 VirusX.py
+python VirusX.py
+fi
+#permainanaj
+if [ $pil = 4 ]
+then
+pkg update && pkg upgrade
+pkg install git
+pkg install python
+pkg install python2
+git clone https://github.com/Err0r-ICA/Ransomware
+cd Ransomware
+fi
+#hdhhdhdhdvvsbhdhd
+if [ $pil = 5 ]
+then
+pkg update && pkg upgrade
+pkg install git
+pkg install python
+pkg install python2
+git clone https://github.com/Zidan-ID17/Virus-Creator
+cd Virus-Creator
+fi
+#hdhdhdb
+if [ $pil = 6 ]
+then
+pkg update && pkg upgrade
+pkg install git
+git clone https://github.com/IntiTutorial/KTP-KK
+cd KTP-KK
+fi
+#hehdh
+if [ $pil = 99 ]
+then
+echo $r"SEMOGA BERUNTUNG YAH MENGGUNAKAN SCRIPT INI"
+sleep 2
+exit
+
+else
+echo $r"mohon maaf commad anda salah"
+sleep 1
+echo $r"ketik dengan benar biar tidak ada masalah"
+sleep 1
+sh p.sh
+fi
